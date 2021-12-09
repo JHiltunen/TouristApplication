4d93490487e7d3674df89ccc985d41b9f3d07596
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-12-09 14:55:45 +0200

    Added SectionedFetchRequest

--- TouristApplication/Models/PlaceSort.swift
+++ TouristApplication/Models/PlaceSort.swift
@@ -15,41 +15,46 @@ struct PlaceSort: Hashable, Identifiable {
     let name: String
     // 4. descriptors are the SortDescriptors to apply.
     let descriptors: [SortDescriptor<PlaceData>]
+    let section: KeyPath<PlaceData, String>
     
     // 1. It adds a static property to allow easy access to sorts from a SwiftUI View.
     static let sorts: [PlaceSort] = [
-      // 2. It returns an array of FriendSort items.
+        // 2. It returns an array of FriendSort items.
         PlaceSort(
-        id: 0,
-        name: "Place name | Ascending",
-        // 3. Each SortDescriptor specifies the keypath to sort on and the order in which to sort. Note the new SortDescriptor API introduces the .forward and .reverse enumeration values instead of the old Boolean-based sort direction specifier. Each option has a primary sort descriptor and a second one for cases where the first value is the same, such as when you meet lots of people in the same place.
-        descriptors: [
-          SortDescriptor(\PlaceData.name, order: .forward)
-          //SortDescriptor(\PlaceData.name, order: .forward)
-        ]),
+            id: 0,
+            name: "Place name | Ascending",
+            // 3. Each SortDescriptor specifies the keypath to sort on and the order in which to sort. Note the new SortDescriptor API introduces the .forward and .reverse enumeration values instead of the old Boolean-based sort direction specifier. Each option has a primary sort descriptor and a second one for cases where the first value is the same, such as when you meet lots of people in the same place.
+            descriptors: [
+                SortDescriptor(\PlaceData.name, order: .forward)
+                //SortDescriptor(\PlaceData.name, order: .forward)
+            ],
+            section: \PlaceData.name),
         PlaceSort(
-        id: 1,
-        name: "Place name | Descending",
-        descriptors: [
-          SortDescriptor(\PlaceData.name, order: .reverse)
-          //SortDescriptor(\PlaceData.name, order: .forward)
-        ]),
+            id: 1,
+            name: "Place name | Descending",
+            descriptors: [
+                SortDescriptor(\PlaceData.name, order: .reverse)
+                //SortDescriptor(\PlaceData.name, order: .forward)
+            ],
+            section: \PlaceData.name),
         PlaceSort(
-        id: 2,
-        name: "Meeting Date | Ascending",
-        descriptors: [
-          SortDescriptor(\PlaceData.name, order: .forward),
-          SortDescriptor(\PlaceData.name, order: .forward)
-        ]),
+            id: 2,
+            name: "Meeting Date | Ascending",
+            descriptors: [
+                SortDescriptor(\PlaceData.name, order: .forward),
+                SortDescriptor(\PlaceData.name, order: .forward)
+            ],
+            section: \PlaceData.name),
         PlaceSort(
-        id: 3,
-        name: "Meeting Date | Descending",
-        descriptors: [
-          SortDescriptor(\PlaceData.name, order: .reverse),
-          SortDescriptor(\PlaceData.name, order: .forward)
-        ])
+            id: 3,
+            name: "Meeting Date | Descending",
+            descriptors: [
+                SortDescriptor(\PlaceData.name, order: .reverse),
+                SortDescriptor(\PlaceData.name, order: .forward)
+            ],
+            section: \PlaceData.name),
     ]
-
+    
     // 4. This static property is used as the initial sort option.
     static var `default`: PlaceSort { sorts[0] }
 }
